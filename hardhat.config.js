df655ca47409dbf62462e0117654257703f3569c
skplunkerin <topher@smoothblack6.com>
2023-01-28 07:43:17 -0700

    * Add "flat" hardhat task to allow support mixed licenses in flattened   contracts
    * Run `npm i` to update lock file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,39 @@
 require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
+// source:
+// https://rubydusa.medium.com/parsererror-multiple-spdx-license-identifiers-found-in-source-file-de4d61ffab64
+task(
+  "flat",
+  "Flattens and prints contracts and their dependencies (Resolves licenses)"
+)
+  .addOptionalVariadicPositionalParam(
+    "files",
+    "The files to flatten",
+    undefined,
+    types.inputFile
+  )
+  .setAction(async ({ files }, hre) => {
+    let flattened = await hre.run("flatten:get-flattened-sources", { files });
+
+    // Remove every line started with "// SPDX-License-Identifier:"
+    flattened = flattened.replace(
+      /SPDX-License-Identifier:/gm,
+      "License-Identifier:"
+    );
+    flattened = `// SPDX-License-Identifier: MIXED\n\n${flattened}`;
+
+    // Remove every line started with "pragma experimental ABIEncoderV2;" except the first one
+    flattened = flattened.replace(
+      /pragma experimental ABIEncoderV2;\n/gm,
+      (
+        (i) => (m) =>
+          !i++ ? m : ""
+      )(0)
+    );
+    console.log(flattened);
+  });
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
@@ -8,6 +41,6 @@ module.exports = {
     goerli: {
       url: process.env.TESTNET_ALCHEMY_RPC_URL,
       accounts: [process.env.TESTNET_WALLET_PRIVATE_KEY],
-    }
-  }
+    },
+  },
 };
